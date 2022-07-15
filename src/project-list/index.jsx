fee9bf8c69e3ace3372db335fc58ddda2fde30fd
liwanbin <2285443668@qq.com>
2022-07-15 12:49:47 +0800

    feat: 添加useMount 和 useDebounce 两个hookk

--- src/project-list/index.jsx
+++ src/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
-import { filterNullValues } from "utils"
+import { filterNullValues, useDebounce, useMount } from "utils"
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -11,23 +11,24 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
+    const debouncedParams = useDebounce(params, 2000)
     const [users, setUsers] = useState([])
     const [list, setList] = useState([])
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
-    const searchParams = new URLSearchParams(filterNullValues(params))
+    })
+    const searchParams = new URLSearchParams(filterNullValues(debouncedParams))
     useEffect(() => {
         fetch(`${apiUrl}/projects?${searchParams.toString()}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [params])
+    }, [debouncedParams])
     return (
         <div>
             <SearchPanel params={params} users={users} setParams={setParams} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export function select(projectObj, users) {
     let newProjectObj = { ...projectObj };
     let projectUser = users.find((user) => user.id === newProjectObj.personId);
@@ -8,7 +10,7 @@ export function select(projectObj, users) {
 function isNullValues(value) {
     return value === 0 ? false : !value;
 }
-
+// 在一个函数里，改变传入的对象本身是不好的
 export function filterNullValues(obj) {
     let newObj = { ...obj };
     Object.keys(newObj).forEach((key) => {
@@ -18,3 +20,20 @@ export function filterNullValues(obj) {
     });
     return newObj;
 }
+
+export const useMount = (cb) => {
+    useEffect(() => {
+        cb();
+    }, []);
+};
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+        return () => clearTimeout(timer);
+    }, [value, delay]);
+    return debouncedValue;
+};
